61e015a96b73c753b9d0e2790747332871291629
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-04 04:44:11 +0900

    feat: CI動くかどうか

--- src/functions/httpTrigger1.ts
+++ src/functions/httpTrigger1.ts
@@ -5,7 +5,7 @@ export async function httpTrigger1(request: HttpRequest, context: InvocationCont
 
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
+    return { body: `Hello v2, ${name}!` };
 };
 
 app.http('httpTrigger1', {

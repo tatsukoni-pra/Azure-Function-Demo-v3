aafafd6bfe8260ec3321ccd67ec23be7d955d742
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-15 11:36:22 +0900

    feat: 実行中断されるかどうか検証2

--- src/functions/timerTrigger1.ts
+++ src/functions/timerTrigger1.ts
@@ -25,7 +25,7 @@ async function addItem(item: any) {
 }
 
 export async function timerTrigger1(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Start timer trigger function v3-1');
+    context.log('Start timer trigger function v3-2');
 
     const recordId = generateRandomNumber().toString();
     context.log('Item will creat with id: ' + recordId);

f50e8a6642d7b73522da7e0b034ced9d7af6ed98
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-16 23:41:26 +0900

    feat: 調整

--- src/functions/cosmosDBTrigger1.ts
+++ src/functions/cosmosDBTrigger1.ts
@@ -12,7 +12,7 @@ const generateRandomNumber: () => number = () => {
 }
 
 export async function cosmosDBTrigger1(documents: unknown[], context: InvocationContext): Promise<void> {
-    const functionVersion = "v3-10";
+    const functionVersion = "v3-11";
     const functionExecId = generateRandomNumber().toString();
     const documentsCount = documents.length;
     if (documentsCount === 0) {

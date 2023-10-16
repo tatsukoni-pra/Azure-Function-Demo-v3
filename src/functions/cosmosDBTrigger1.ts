933cd60f07fffd923b7b64f81d3f7a3277f145f5
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-10-16 22:02:08 +0900

    feat: 関数の実行時間を伸ばす

--- src/functions/cosmosDBTrigger1.ts
+++ src/functions/cosmosDBTrigger1.ts
@@ -19,7 +19,7 @@ const generateRandomNumber: () => number = () => {
 }
 
 export async function cosmosDBTrigger1(documents: unknown[], context: InvocationContext): Promise<void> {
-    const functionVersion = "v3-7";
+    const functionVersion = "v3-8";
     const functionExecId = generateRandomNumber().toString();
     const documentsCount = documents.length;
     if (documentsCount === 0) {
